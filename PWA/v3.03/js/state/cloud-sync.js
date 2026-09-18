0d62a0d7b01c8f749e204783e7ae75e56de97c47
aqcroft <aqcroft@gmail.com>
2026-09-18 22:46:01 +0100

    Isolate V3.03 Cloud session

--- PWA/v3.03/js/state/cloud-sync.js
+++ PWA/v3.03/js/state/cloud-sync.js
@@ -4,7 +4,7 @@ import { clone } from './canonical-state.js';
 import { migrateAppointment, toLegacyCompatibleAppointment } from './migrations.js';
 import { reconcileSnapshots, remoteVersion } from './reconciliation.js';
 
-export const AUTH_KEY = 'apptCompanionV301CloudAuthSession';
+export const AUTH_KEY = 'apptCompanionV303CloudAuthSession';
 
 export function getCloudAuth() {
   try {
