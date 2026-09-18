58bfaac4f2e2fae3875f0452a7ae9fa803bcc2d8
aqcroft <aqcroft@gmail.com>
2026-09-18 22:46:06 +0100

    Set V3.03 version identity

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.01-ui.1';
+export const VERSION = '3.03-ui.1';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v301-20260918-1';
+export const CACHE_VERSION = 'appointment-companion-v303-20260918-1';
