aa36e4f8879dda37b4a4dbd10f4155b1b1ed886a
aqcroft <aqcroft@gmail.com>
2026-09-19 12:31:44 +0100

    Bump V3.03 Broadband flow version

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.6';
+export const VERSION = '3.03-ui.7';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260919-6';
+export const CACHE_VERSION = 'appointment-companion-v303-20260919-7';
