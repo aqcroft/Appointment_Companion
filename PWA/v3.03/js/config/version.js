63bd0c4c727c5a2c6801983bcc6bf0e7daf0ec35
aqcroft <aqcroft@gmail.com>
2026-09-19 12:27:37 +0100

    Bump V3.03 Energy sequence version

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.5';
+export const VERSION = '3.03-ui.6';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260919-5';
+export const CACHE_VERSION = 'appointment-companion-v303-20260919-6';
