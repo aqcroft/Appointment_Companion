61084eab7d4acc15a8c43910818ed54eaadef06f
aqcroft <aqcroft@gmail.com>
2026-09-19 06:24:51 +0100

    Bump V3.03 cache identity

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.1';
+export const VERSION = '3.03-ui.2';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260918-1';
+export const CACHE_VERSION = 'appointment-companion-v303-20260919-2';
