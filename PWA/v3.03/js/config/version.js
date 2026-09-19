9269bfa2fdce598f84acd56fd9a2f0721a0af8d9
aqcroft <aqcroft@gmail.com>
2026-09-19 08:02:33 +0100

    Bump V3.03 figures-first version

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.2';
+export const VERSION = '3.03-ui.3';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260919-2';
+export const CACHE_VERSION = 'appointment-companion-v303-20260919-3';
