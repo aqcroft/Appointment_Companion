ecf2460e6d54e5f4f48fc1f1bf201bb655c4adce
aqcroft <aqcroft@gmail.com>
2026-09-19 12:17:24 +0100

    Bump V3.03 usage-modal version

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.4';
+export const VERSION = '3.03-ui.5';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260919-4';
+export const CACHE_VERSION = 'appointment-companion-v303-20260919-5';
