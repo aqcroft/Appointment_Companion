b0772abdeb12a3c9ed16bcba99a413417789e225
aqcroft <aqcroft@gmail.com>
2026-09-24 23:57:26 +0100

    Bump v3 cache for notification preferences

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.12';
+export const VERSION = '3.03-ui.13';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260924-12';
+export const CACHE_VERSION = 'appointment-companion-v303-20260924-13';
