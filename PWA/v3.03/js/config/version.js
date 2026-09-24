e2f18cc1a35c2b6185388f3af593d2b117c66b14
aqcroft <aqcroft@gmail.com>
2026-09-24 23:33:49 +0100

    Bump v3.03 cache for auth migration

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.11';
+export const VERSION = '3.03-ui.12';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260924-11';
+export const CACHE_VERSION = 'appointment-companion-v303-20260924-12';
