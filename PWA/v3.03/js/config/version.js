992df2ed0683affeccff44274bfc5a414f149163
aqcroft <aqcroft@gmail.com>
2026-09-25 08:19:51 +0100

    Bump v3.03 version for tariff refresh success

--- PWA/v3.03/js/config/version.js
+++ PWA/v3.03/js/config/version.js
@@ -1,3 +1,3 @@
-export const VERSION = '3.03-ui.14';
+export const VERSION = '3.03-ui.15';
 export const RULE_VERSION = '2026-10-01';
-export const CACHE_VERSION = 'appointment-companion-v303-20260925-14';
+export const CACHE_VERSION = 'appointment-companion-v303-20260925-15';
